ef0e5ef6e81a774bcfeb9002a69a11f44f66876b
Roaa2002-web <roaa.abdulfattah.alwesabi@gmail.com>
2024-09-04 12:10:29 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import './App.css'; // استيراد ملف CSS
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => setDogImage(data.message));
+  }, []);
+
+  return (
+    <div>
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
